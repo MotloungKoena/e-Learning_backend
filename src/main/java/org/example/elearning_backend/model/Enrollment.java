6131707f28729a643bb93692e349e9e6eaefa2f2
Koena Motloung <motloungkoena@gmail.com>
2026-03-24 08:44:16 +0200

    updated Enrollment

--- src/main/java/org/example/elearning_backend/model/Enrollment.java
+++ src/main/java/org/example/elearning_backend/model/Enrollment.java
@@ -1,6 +1,6 @@
 package org.example.elearning_backend.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;  // Add this import
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,12 +20,12 @@ public class Enrollment {
 
     @ManyToOne
     @JoinColumn(name = "student_id")
-    @JsonIgnoreProperties({"enrollments", "password"})  // Ignore these fields when serializing
+    @JsonIgnoreProperties({"enrollments", "password"})
     private User student;
 
     @ManyToOne
     @JoinColumn(name = "course_id")
-    @JsonIgnoreProperties({"enrollments", "instructor"})  // Ignore these fields
+    @JsonIgnoreProperties({"enrollments", "instructor"})
     private Course course;
 
     @Column(name = "enrolled_at")
@@ -42,48 +42,3 @@ public class Enrollment {
         completed = false;
     }
 }
-
-
-/*package org.example.elearning_backend.model;
-
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "enrollments")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Enrollment {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    // Many enrollments can belong to one student
-    @ManyToOne
-    @JoinColumn(name = "student_id")
-    private User student;
-
-    // Many enrollments can be for one course
-    @ManyToOne
-    @JoinColumn(name = "course_id")
-    private Course course;
-
-    @Column(name = "enrolled_at")
-    private LocalDateTime enrolledAt;
-
-    private Integer progress;  // 0 to 100 percentage
-
-    private Boolean completed;
-
-    @PrePersist
-    protected void onCreate() {
-        enrolledAt = LocalDateTime.now();
-        progress = 0;
-        completed = false;
-    }
-}*/
